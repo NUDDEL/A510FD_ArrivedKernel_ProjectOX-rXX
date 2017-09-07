819cb637fb01e10731e5fa8f868421683481235c
NUDDEL <devnuddel@gmail.com>
2017-09-07 20:58:15 +0200

    Project OX r20.3

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Sep 7 20:47:26 CEST 2017"
+#define LINUX_COMPILE_BY "nuddel"
+#define LINUX_COMPILE_HOST "Lubuntu-vServer"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
